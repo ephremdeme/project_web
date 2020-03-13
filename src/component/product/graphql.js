6edd175ae759f1a8e668ca3c08a629e9423e983e
Ephrem Demelash <demelashephrem@gmail.com>
2020-03-13 21:36:47 +0300

    add graphql  request

---
+++ src/component/product/graphql.js
@@ -0,0 +1,109 @@
+import { gql } from "apollo-boost";
+
+export const UPLOAD_IMAGE = gql`
+  mutation uploadFile($file: Upload!, $productId: Int!) {
+    uploadImage(file: $file, productId: $productId) {
+      id
+      filename
+    }
+  }
+`;
+export const ADD_PRODUCT = gql`
+  mutation addProduct(
+    $name: String!
+    $price: Float!
+    $quantity: Int!
+    $description: String!
+    $categoryId: Int!
+    $image: Upload!
+  ) {
+    createProduct(
+      name: $name
+      price: $price
+      quantity: $quantity
+      description: $description
+      categoryId: $categoryId
+      file: $image
+    ) {
+      id
+      name
+      price
+      quantity
+      description
+      images {
+        filename
+      }
+    }
+  }
+`;
+export const UPDATE_PRODUCT = gql`
+  mutation editProduct(
+    $name: String!
+    $price: Float!
+    $quantity: Int!
+    $description: String!
+    $categoryId: Int!
+    $image: Upload!
+  ) {
+    updateProduct(
+      name: $name
+      price: $price
+      quantity: $quantity
+      description: $description
+      categoryId: $categoryId
+      file: $image
+    ) {
+      id
+      name
+      price
+      quantity
+      description
+      images {
+        filename
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCT = gql`
+  query getProduct($id: Int!) {
+    product(id: $id) {
+      id
+      name
+      price
+      comments {
+        id
+        comment
+      }
+      images {
+        filename
+      }
+      rating {
+        count
+        rating
+      }
+    }
+  }
+`;
+
+export const GET_PRODUCTS = gql`
+  {
+    products {
+      id
+      name
+      price
+      images {
+        filename
+      }
+    }
+  }
+`;
+
+export const GET_CATEGORY = gql`
+{
+  categories{
+    id
+    category
+  }
+}
+`
