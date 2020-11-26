8aed0f41346085ccd7efc074e1c0dd1741c43cab
Ephrem Demelash <demelashephrem@gmail.com>
2020-11-27 00:50:48 +0300

    cofigures it to include apollo client v3

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-import React from "react";
+import React, { Suspense, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import AppNavbar from "./layout/AppNavBar";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import LoginPage from "./component/Auth/Login";
-import RegisterPage from "./component/Auth/Register";
-import FeaturesPage from "./layout/intro";
-import ProductPage from "./component/product";
-import AddProduct from "./component/product/add";
-import ProductShow from "./component/product/show";
-import TestRegister from "./component/Auth/test";
-import ProductCheckout, { ProductCart } from "./component/product/cart";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import { ToastProvider } from "react-toast-notifications";
+import { BreadcrumbsProvider } from "react-breadcrumbs-dynamic";
+import ScrollToTop from "./helpers/scroll-top";
+import AppHeader from "./layout/AppHeader";
+import Login from "./pages/auths/LogIn";
+import AppRegister from "./pages/auths/Register";
+import { GET_USER } from "./pages/auths/graphql";
+import { useQuery, useReactiveVar } from "@apollo/client";
+import { authDataVar } from "./authReactive";
 
 function App() {
+  const { error, loading, data } = useQuery(GET_USER);
+  const ss = useReactiveVar(authDataVar);
+  useEffect(() => {
+    if (data) {
+      authDataVar({
+        ...ss,
+        id: data?.getUser?.id,
+        username: data?.getUser?.username,
+      });
+    }
+  }, [data]);
+
   return (
     <React.Fragment>
-      <Router>
-        <div className="flexible-content">
-          <AppNavbar />
-          <main className="my-5">
-            <Route path="/login" component={LoginPage} />
-            <Route path="/register" component={TestRegister} />
-            <Route path="/products" exact component={ProductPage} />
-            <Route path="/products/cart" exact component={ProductCart} />
-            <Route path="/products/checkout" exact component={ProductCheckout} />
-            <Route path="/products/add" component={AddProduct} />
-            <Route path="/products/:id" component={ProductShow} />
-            <Route path="/" exact component={FeaturesPage} />
-          </main>
-        </div>
-      </Router>
+      <ToastProvider placement="bottom-left">
+        <BreadcrumbsProvider>
+          <Router>
+            <ScrollToTop>
+              <Suspense
+                fallback={
+                  <div className="flone-preloader-wrapper">
+                    <div className="flone-preloader">
+                      <span></span>
+                      <span></span>
+                    </div>
+                  </div>
+                }
+              >
+                <AppHeader />
+                <Switch>
+                  <Route exact path="/" component={Login} />
+                  <Route exact path="/login" component={Login} />
+                  <Route exact path="/register" component={AppRegister} />
+                </Switch>
+              </Suspense>
+            </ScrollToTop>
+          </Router>
+        </BreadcrumbsProvider>
+      </ToastProvider>
     </React.Fragment>
   );
 }
