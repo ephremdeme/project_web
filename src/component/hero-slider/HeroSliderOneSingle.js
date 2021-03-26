80bfe21ca0c6afed6dc6894bca2f323507551d42
ephremdeme <demelashephrem@gmail.com>
2021-03-26 15:59:27 +0300

    🐛 FIX: fix hero slider layout and improve ui

--- src/component/hero-slider/HeroSliderOneSingle.js
+++ src/component/hero-slider/HeroSliderOneSingle.js
@@ -5,19 +5,21 @@ import { Link } from "react-router-dom";
 const HeroSliderOneSingle = ({ data, sliderClassName }) => {
   return (
     <div
-      className={`single-slider slider-height-1 bg-purple ${
+      className={`single-slider image-slider slider-height-1 bg-light-gray ${
         sliderClassName ? sliderClassName : ""
       }`}
     >
-      <div className="container">
+      <div className="container-fluid">
         <div className="row">
           <div className="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
-            <div className="slider-content slider-animated-1">
-              <h1 className="animated">{data.title}</h1>
-              <h3 className="animated">{data.subtitle}</h3>
+            <div className="slider-content slider-animated-1 text-center animation-text">
+              <h1 className="animated font-weight-bold  dark-grey-text">
+                {data.title}
+              </h1>
+              <h3 className="animated ">{data.subtitle}</h3>
               <div className="slider-btn btn-hover mt-4">
                 <Link
-                  className="animated"
+                  className="animated font-weight-bold"
                   to={process.env.PUBLIC_URL + data.url}
                 >
                   SHOP NOW
@@ -26,9 +28,9 @@ const HeroSliderOneSingle = ({ data, sliderClassName }) => {
             </div>
           </div>
           <div className="col-xl-6 col-lg-6 col-md-6 col-12 col-sm-6">
-            <div className="slider-single-img slider-animated-1">
+            <div className=" slider-animated-1">
               <img
-                className="animated img-fluid mb-2"
+                className="animated img-fluid mb-2 rounded z-depth-2"
                 src={process.env.PUBLIC_URL + data.image}
                 alt=""
               />
